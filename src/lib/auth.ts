f5b3621131d5e95574499f6204cf95d065ec6a4d
Najm Hoda <najmkhoda@gmail.com>
2025-03-02 12:22:26 -0800

    Add error state for auth functions

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,40 +2,51 @@
 
 import bcrypt from 'bcrypt'
 import { User } from './database';
-import { createSession, getUser } from './session';
+import { createSession } from './session';
+import { redirect } from 'next/navigation';
 
 const PWD_SALT_ROUNDS = Number(process.env.PWD_SALT_ROUNDS!);
 
 export async function handleLogin(username: string, password: string) {
-    const user = await User
-        .findOne({ username })
-        .select('password')
-        .lean();
-    if (!user) return;
-    
-    const passwordsMatch = await bcrypt.compare(password, user.password);
-    if (!passwordsMatch) return;
-    
-    await createSession(user._id.toString());
+    try {
+        const user = await User
+            .findOne({ username })
+            .select('password')
+            .lean();
+        if (!user) return 'invalid-credentials';
+        
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+        if (!passwordsMatch) return 'invalid-credentials';
+        
+        await createSession(user._id.toString());
+        redirect('/dashboard');
+    } catch {
+        return 'server-error';
+    }
 }
 
 export async function handleSignup(username: string, password: string, email: string) {
-    const existingUser = await User.exists({ 
-        $or: [{ username }, { email }]
-    });
-    if (existingUser) return;
-    
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, PWD_SALT_ROUNDS);
-    
-    // Create new user
-    const newUser = await User.create({
-        username,
-        password: hashedPassword,
-        email,
-    });
-    
-    // Create session for new user
-    await createSession(newUser._id.toString());
-}
+    try {
+        const existingUsername = await User.exists({ username });
+        if (existingUsername) return 'username-taken';
 
+        const existingEmail = await User.exists({ email });
+        if (existingEmail) return 'email-taken';
+        
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, PWD_SALT_ROUNDS);
+        
+        // Create new user
+        const newUser = await User.create({
+            username,
+            password: hashedPassword,
+            email
+        });
+        
+        // Create session for new user
+        await createSession(newUser._id.toString());
+        redirect('/dashboard');
+    } catch {
+        return 'server-error';
+    }
+}
