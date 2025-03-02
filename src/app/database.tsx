2becca929727bde4898a3da96acab058e84128c5
Gavin <gsag@ucla.edu>
2025-03-02 10:36:08 -0800

    Working on database

--- src/app/database.tsx
+++ src/app/database.tsx
@@ -1,1 +1,29 @@
-const mongoose = require('mongoose')
+import mongoose, { Schema, Document } from 'mongoose'
+
+interface IMessage {
+    senderId: mongoose.Types.ObjectId,
+    contents: string
+    timestamp: Date
+}
+
+interface IPost extends Document {
+    _id: mongoose.Types.ObjectId,
+    posterId: mongoose.Types.ObjectId, // user that is posting
+    responderId: mongoose.Types.ObjectId, // user that responds/picks up (will be $unset if not set)
+    messages: IMessage[]
+}
+
+interface IUser extends Document {
+    _id: mongoose.Types.ObjectId,
+    username: string,
+    email: string,
+    posts: IPost[]
+}
+
+const userSchema: Schema<IUser> = new Schema(
+    {
+        username: { type: String, required: true },
+        email: { type: String, required: true },
+        posts: { type: IPost[], required: true }
+    }
+)
